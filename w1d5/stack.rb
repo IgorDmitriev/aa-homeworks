46129e21d888e79a9770105012b9bac902a272f9
Igor Dmitriev <igor.s.dmitriev@gmail.com>
2016-11-18 07:09:48 -0800

    Removes comments and reader access for instance var

--- w1d5/stack.rb
+++ w1d5/stack.rb
@@ -1,24 +1,18 @@
 class Stack
-  attr_reader :stack
-
    def initialize
-     # create ivar to store stack here!
      @stack = []
    end
 
    def add(el)
-     # adds an element to the stack
-     stack << el
+     @stack << el
      el
    end
 
    def remove
-     # removes one element from the stack
-     stack.pop
+     @stack.pop
    end
 
    def show
-     # return a copy of the stack
-     stack.dup
+     @stack.dup
    end
  end
