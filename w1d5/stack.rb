cbb66c8abd5c0d0d7eb262faefe09979feb1d032
Igor Dmitriev <igor.s.dmitriev@gmail.com>
2016-11-18 07:04:11 -0800

    Creates Stack base methods

--- w1d5/stack.rb
+++ w1d5/stack.rb
@@ -1,17 +1,24 @@
 class Stack
+  attr_reader :stack
+
    def initialize
      # create ivar to store stack here!
+     @stack = []
    end
 
    def add(el)
      # adds an element to the stack
+     stack << el
+     el
    end
 
    def remove
      # removes one element from the stack
+     stack.pop
    end
 
    def show
      # return a copy of the stack
+     stack.dup
    end
  end
